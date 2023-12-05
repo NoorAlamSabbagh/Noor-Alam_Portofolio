18934b694403e93340e7a19029aea12217109769
NoorAlamSabbagh <alamrangrej787@gmail.com>
2023-12-05 17:04:54 +0530

    setup

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+function Hero() {
+  return (
+    <div className='hero'>
+      <div className="imageContainer">
+        <img src='/hero.png' alt=''/>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
